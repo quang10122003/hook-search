69109c7567ccce154d43a36eb6620dec1b4fe689
quang10122003 <qdao10122003badao@gmail.com>
2026-03-25 15:14:25 +0700

    feat: config for next-intl

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,11 +2,12 @@
 import Header from "@/components/Header";
 import HookDesc from "@/components/HookDesc";
 import InputHook from "@/components/SearchBox";
-import { HookContext } from "../context/HookContext";
+import { HookContext } from "../../context/HookContext";
 import { useContext } from "react";
 import { Tab } from "@/components/Tab";
-
+import { useTranslations } from "next-intl";
 export default function Home() {
+  const t = useTranslations('HomePage');
   return (
     <>
       <Tab />
