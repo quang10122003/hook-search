3213eb24352c391724a675b4f08e55ded8955024
quang10122003 <qdao10122003badao@gmail.com>
2026-03-25 17:34:42 +0700

    feat:Add an interface to select the language.

--- components/LanguageSwitcher.tsx
+++ components/LanguageSwitcher.tsx
@@ -1,28 +1,31 @@
 "use client";
 
-import { useLocale } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 import { usePathname, useRouter } from "@/i18n/navigation";
+import styles from "../style/LanguageSwitcher.module.css";
 
 export default function LanguageSwitcher() {
-  const locale = useLocale();
-  const pathname = usePathname();
-  const router = useRouter();
+    const locale = useLocale();
+    const pathname = usePathname();
+    const router = useRouter();
+    const t = useTranslations("LanguageSwitcher")
 
-  function handleChange(value: string) {
-    const nextLocale = value;
-    router.replace(pathname, { locale: nextLocale });
-  }
+    function handleChange(value: string) {
+        const nextLocale = value;
+        router.replace(pathname, { locale: nextLocale });
+    }
 
-  return (
-    <div>
-      <label >Language: </label>
-      <select
-        value={locale}
-        onChange={(e) => handleChange(e.target.value)}
-      >
-        <option value="vn">Vietnamese</option>
-        <option value="en">English</option>
-      </select>
-    </div>
-  );
+    return (
+        <div className={styles.languageSwitcher}>
+            <label className={styles.languageSwitcher__label}>{t("languageSwitcher__label")}:</label>
+            <select
+                className={styles.languageSwitcher__select}
+                value={locale}
+                onChange={(e) => handleChange(e.target.value)}
+            >
+                <option className={styles.languageSwitcher__option} value="vn">{t("languageSwitcher__option")}</option>
+                <option className={styles.languageSwitcher__option} value="en">{t("languageSwitcher__option2")}</option>
+            </select>
+        </div>
+    );
 }
